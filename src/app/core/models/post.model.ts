2534498da8debec401c138f5f10f7ecddc005f05
oarias93 <eduardo93-12@hotmail.com>
2025-04-23 13:37:01 -0500

    chore: Configurar el servicio para conectarse a la API publica

---
+++ src/app/core/models/post.model.ts
@@ -0,0 +1,12 @@
+export interface Post {
+  userId: number;  // ID del usuario que creó el post
+  id: number;     // ID único del post
+  title: string;  // Título del post
+  body: string;   // Contenido del post
+}
+
+export type ApiState<T> = {
+  loading: boolean;
+  data?: T;
+  error?: string;
+};
--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../models/post.model';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Singleton en toda la app
 })
 export class PostService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor() { }
+  constructor(private http: HttpClient) {} // Inyección de HttpClient
+
+  // Trae todos los posts
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl).pipe(
+      catchError(this.handleError<Post[]>('getPosts', []))
+    );
+  }
+
+  // Trae un post por su id
+  getPostById(id: number): Observable<Post> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Post>(url).pipe(
+      catchError(this.handleError<Post>(`getPostById id=${id}`))
+    );
+  }
+
+  //Manejo de errores
+  // El método handleError maneja errores de las peticiones HTTP
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`);
+      return throwError(() => new Error('Error al cargar los datos. Inténtalo de nuevo.'));
+    };
+  }
+
 }
