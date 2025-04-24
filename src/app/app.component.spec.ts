1da44e8759f102b8b8b31659a048706de0c6620c
oarias93 <eduardo93-12@hotmail.com>
2025-04-23 20:47:03 -0500

    SCSS ajustados para dar una visual mas fresca

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,6 +19,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Prueba Técnica');
+    expect(compiled.querySelector('h1')?.textContent).toContain('PRUEBA TÉCNICA');
   });
 });
